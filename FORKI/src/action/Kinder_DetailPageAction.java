bc00180eb51177de7669ae9c087bf33d5a95a6b6
Edward <Edward@Cochineal>
2016-08-11 12:57:54 +0900

    commit

--- FORKI/src/action/Kinder_DetailPageAction.java
+++ FORKI/src/action/Kinder_DetailPageAction.java
@@ -1,5 +1,16 @@
 package action;
 
-public class Kinder_DetailPageAction {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Kinder_DetailPageAction implements CommandAction {
+
+	@Override
+	public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable {
+		
+		request.setCharacterEncoding("UTF-8");
+		
+		return "kinder_DetailPage.jsp";
+	}
 
 }
