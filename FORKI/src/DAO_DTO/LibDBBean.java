608927f5b17800351d06c775f745ca71b54ec2a5
user1 <user1@user1-PC>
2016-08-23 14:19:33 +0900

    0823 현아 학원

--- FORKI/src/DAO_DTO/LibDBBean.java
+++ FORKI/src/DAO_DTO/LibDBBean.java
@@ -34,6 +34,14 @@ public class LibDBBean {
 		String[] gu={"전체","강남구","강동구","강북구","강서구","관악구","광진구","구로구","금천구","노원구","도봉구","동대문구","동작구","마포구","서대문구","서초구","성동구","성북구","송파구","양천구","영등포구","용산구","은평구","종로구","중랑구","중구"};
 		try{
 			conn = getConnection();
+			if(gu_nm == 0){
+				if(searchN==null){
+				pstmt = conn.prepareStatement("select * from tourism_list");
+				}else{
+				pstmt = conn.prepareStatement("select * from tourism_list where park_nm like '%"+searchN+"%'");	
+				}
+			}
+			else{
 			if(dong==null && searchN==null){
 			pstmt = conn.prepareStatement("select * from tourism_list where gu_nm='"+gu[gu_nm]+"'");
 			}
@@ -45,6 +53,7 @@ public class LibDBBean {
 			}
 			else if(dong != null && searchN != null){
 			pstmt = conn.prepareStatement("select * from tourism_list where gu_nm='"+gu[gu_nm]+"' and adres like'%"+dong+ "%' and park_nm like '%"+searchN+"%'")	;
+			}
 			}
 			rs = pstmt.executeQuery();
 			if(rs.next()){
@@ -86,20 +95,27 @@ public class LibDBBean {
 		String[] gu={"전체","강남구","강동구","강북구","강서구","관악구","광진구","구로구","금천구","노원구","도봉구","동대문구","동작구","마포구","서대문구","서초구","성동구","성북구","송파구","양천구","영등포구","용산구","은평구","종로구","중랑구","중구"};
 		try{
 			conn = getConnection();
-			
-			if(dong==null && searchN==null){
-			pstmt = conn.prepareStatement("select * from  pub_lib where gu_nm='"+gu[gu_nm]+"'");
+			if(gu_nm == 0){
+				if(searchN==null){
+				pstmt = conn.prepareStatement("select * from pub_lib");
+				}else{
+				pstmt = conn.prepareStatement("select * from pub_lib where libry_name like '%"+searchN+"%'");	
+				}
 			}
-			else if(dong == null && searchN != null){
-			pstmt = conn.prepareStatement("select * from pub_lib where gu_nm='"+gu[gu_nm]+"' and libry_name like '%"+searchN+"%'");	
+			else{
+				if(dong==null && searchN==null){
+					pstmt = conn.prepareStatement("select * from  pub_lib where gu_nm='"+gu[gu_nm]+"'");
+				}
+				else if(dong == null && searchN != null){
+					pstmt = conn.prepareStatement("select * from pub_lib where gu_nm='"+gu[gu_nm]+"' and libry_name like '%"+searchN+"%'");	
+					}
+				else if(dong != null && searchN ==null){
+					pstmt = conn.prepareStatement("select * from pub_lib where gu_nm='"+gu[gu_nm]+"' and adres like'%"+dong+"%'");	
+				}
+				else if(dong != null && searchN != null){
+					pstmt = conn.prepareStatement("select * from pub_lib where gu_nm='"+gu[gu_nm]+"' and adres like'%"+dong+ "%' and libry_name like '%"+searchN+"%'")	;
+				}
 			}
-			else if(dong != null && searchN ==null){
-			pstmt = conn.prepareStatement("select * from pub_lib where gu_nm='"+gu[gu_nm]+"' and adres like'%"+dong+"%'");	
-			}
-			else if(dong != null && searchN != null){
-			pstmt = conn.prepareStatement("select * from pub_lib where gu_nm='"+gu[gu_nm]+"' and adres like'%"+dong+ "%' and libry_name like '%"+searchN+"%'")	;
-			}
-			
 			rs = pstmt.executeQuery();
 			if(rs.next()){
 				vt = new Vector();
@@ -141,17 +157,26 @@ public class LibDBBean {
 		String[] gu={"전체","강남구","강동구","강북구","강서구","관악구","광진구","구로구","금천구","노원구","도봉구","동대문구","동작구","마포구","서대문구","서초구","성동구","성북구","송파구","양천구","영등포구","용산구","은평구","종로구","중랑구","중구"};
 		try{
 			conn = getConnection();
-			if(dong==null && searchN==null){
-			pstmt = conn.prepareStatement("select * from toy_lib where gu_nm='"+gu[gu_nm]+"'");
+			if(gu_nm == 0){
+				if(searchN==null){
+				pstmt = conn.prepareStatement("select * from toy_lib");
+				}else{
+				pstmt = conn.prepareStatement("select * from toy_lib where park_nm like '%"+searchN+"%'");	
+				}
 			}
-			else if(dong == null && searchN != null){
-			pstmt = conn.prepareStatement("select * from toy_lib where gu_nm='"+gu[gu_nm]+"' and lib_nm like '%"+searchN+"%'");	
-			}
-			else if(dong != null && searchN ==null){
-			pstmt = conn.prepareStatement("select * from toy_lib where gu_nm='"+gu[gu_nm]+"' and adres like'%"+dong+"%'");	
-			}
-			else if(dong != null && searchN != null){
-			pstmt = conn.prepareStatement("select * from toy_lib where gu_nm='"+gu[gu_nm]+"' and adres like'%"+dong+ "%' and lib_nm like '%"+searchN+"%'")	;
+			else{
+				if(dong==null && searchN==null){
+					pstmt = conn.prepareStatement("select * from toy_lib where gu_nm='"+gu[gu_nm]+"'");
+				}
+				else if(dong == null && searchN != null){
+					pstmt = conn.prepareStatement("select * from toy_lib where gu_nm='"+gu[gu_nm]+"' and lib_nm like '%"+searchN+"%'");	
+				}
+				else if(dong != null && searchN ==null){
+					pstmt = conn.prepareStatement("select * from toy_lib where gu_nm='"+gu[gu_nm]+"' and adres like'%"+dong+"%'");	
+				}
+				else if(dong != null && searchN != null){
+					pstmt = conn.prepareStatement("select * from toy_lib where gu_nm='"+gu[gu_nm]+"' and adres like'%"+dong+ "%' and lib_nm like '%"+searchN+"%'")	;
+				}
 			}
 			rs = pstmt.executeQuery();
 			if(rs.next()){
